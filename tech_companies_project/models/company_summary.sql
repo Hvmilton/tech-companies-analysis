d337e634fb6121f938d7ce7ac52604b86caa1568
Josh <jhvmilton@gmail.com>
2024-08-23 17:07:42 -0700

    Added filter for Software industry in dbt model

--- tech_companies_project/models/company_summary.sql
+++ tech_companies_project/models/company_summary.sql
@@ -3,4 +3,5 @@ SELECT
   COUNT(*) AS company_count,
   AVG(CAST("Market Cap" AS DOUBLE)) AS avg_market_cap
 FROM {{ source('tech_companies_source', 'tech_companies') }}
+WHERE industry = 'Software'
 GROUP BY industry
