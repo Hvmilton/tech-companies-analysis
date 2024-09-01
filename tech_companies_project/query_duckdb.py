cbe9a84362a9487682021e6cbc8281884be1fc8b
Josh <jhvmilton@gmail.com>
2024-08-31 18:33:08 -0700

    updated query script for docker

--- tech_companies_project/query_duckdb.py
+++ tech_companies_project/query_duckdb.py
@@ -1,7 +1,9 @@
 import duckdb
 
-# Connect to your DuckDB database
-conn = duckdb.connect('C:/Users/jhvmi/Documents/GitHub/tech-companies-analysis/tech_companies_project/tech_companies.duckdb')
+# Connect to your DuckDB database (first one for local, second for container)
+# conn = duckdb.connect('C:/Users/jhvmi/Documents/GitHub/tech-companies-analysis/tech_companies_project/tech_companies.duckdb')
+
+conn = duckdb.connect('/app/tech_companies.duckdb')
 
 # query
 result = conn.execute("SELECT * FROM tech_companies LIMIT 10").fetchall()
